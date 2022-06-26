37b093798477cfe526aa4d9c79eb8e9dbaa99a5e
Dara Kosova <darakosova.gb@gmail.com>
2022-06-26 16:14:02 +0400

    Добавлен второй метод ко 2-й задаче

---
+++ intern_3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Второй метод решения задачи 2
+//Написать программу, которая будет выдавать название дня недели по заданному номеру
+//3-> Среда
+//5-> Пятница
+
+Console.WriteLine("Введите число дня недели от 1-го до 7-ми: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+
+{
+    case 1: Console.WriteLine("Понедельник");
+    break;
+    case 2: Console.WriteLine("Вторник");
+    break;
+    case 3: Console.WriteLine("Среда");
+    break;
+    case 4: Console.WriteLine("Четверг");
+    break;
+    case 5: Console.WriteLine("Пятница");
+    break;
+    case 6: Console.WriteLine("Суббота");
+    break;
+    case 7: Console.WriteLine("Воскресение");
+    break;
+}
